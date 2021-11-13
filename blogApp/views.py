97ec6940162ce7bd61b9c9d4705783fa4747a76d
Me <jlhofstra@yahoo.com>
2021-11-14 02:43:09 +0900

    posts show up on home page

--- blogApp/views.py
+++ blogApp/views.py
@@ -1,4 +1,12 @@
 from django.shortcuts import render
+from .models import Post
+from django.views.generic import ListView, DetailView
 
-def home(request):
-    return render(request, 'home.html', )
+
+# def home(request):
+#     return render(request, 'home.html', )
+
+class HomeView(ListView):
+    model = Post
+    template_name = 'home.html'
+    
